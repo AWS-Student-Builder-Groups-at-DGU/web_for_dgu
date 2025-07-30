32b0a5ae28a9fd9b9ef2c92c8ff9c376fb062330
devbini <cong8685@naver.com>
2025-07-31 01:23:32 +0900

    fix: Header 배경 색상 추가 및 META 수정

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -23,7 +23,7 @@ export function Header() {
     }
 
     return (
-        <header className="fixed top-0 w-full border-b border-gray-200 z-50">
+        <header className="fixed top-0 w-full border-b border-gray-200 bg-white z-50">
             <div className="container mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex justify-between items-center h-16">
 
