0baba949c4e43577611b51d81a0862c23805ca73
devbini <cong8685@naver.com>
2026-01-26 23:04:52 +0900

    feat: web-metadata 추가
    트위터 대응으로 만들긴 했는데, 이게 x에서 될 지는...

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,30 @@ export const metadata: Metadata = {
   title: 'AWS Cloud Clubs at DGU',
   description:
     '미래의 클라우드 리더를 양성하는 AWS Cloud Clubs at DGU 공식 홈페이지입니다.',
+  openGraph: {
+    title: 'AWS Cloud Clubs at DGU',
+    description:
+      'AWS Cloud Clubs at DGU 공식 홈페이지입니다.',
+    url: 'https://acc-dgu.com/',
+    siteName: 'AWS Cloud Club at DGU',
+    images: [
+      {
+        url: process.env.NEXT_PUBLIC_S3_URL + '/og-image.png',
+        width: 1200,
+        height: 630,
+        alt: 'AWS Cloud Clubs at DGU 공식 로고',
+      },
+    ],
+    locale: 'ko_KR',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'AWS Cloud Clubs at DGU',
+    description:
+      'AWS Cloud Clubs at DGU 공식 홈페이지입니다.',
+    images: [process.env.NEXT_PUBLIC_S3_URL + '/og-image.png'],
+  },
 };
 
 export default function RootLayout({
